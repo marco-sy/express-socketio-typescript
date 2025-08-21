fed7bbf480f9b98d06d8720fb0a074d27e6d3d40
marco-sy <marcosullcayauri@gmail.com>
2025-08-20 22:23:17 -0500

    feat/ copyfiles index.html to /dist

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const server = createServer(app);
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
+  res.sendFile(join(__dirname, "public/index.html"));
 });
 
 io.on("connection", (socket) => {
