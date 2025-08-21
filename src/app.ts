dde34f4154f21c9f44dc23bbcae3e02d257ba544
marco-sy <marcosullcayauri@gmail.com>
2025-08-20 21:50:38 -0500

    first base code socket.io

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { join } from "path";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  // socket.broadcast.emit("chat message", "");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("Message: " + msg);
+    socket.broadcast.emit("chat message", msg);
+  });
+});
+
+export default server;
